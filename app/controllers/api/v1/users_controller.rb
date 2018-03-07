a22baa33070c5ee7dea7a157af291502dcc47d3b
KSheehan <ksheehan927@gmail.com>
2018-03-06 22:42:46 -0500

    Initial commit. User model and controller setup

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,48 @@
+module Api
+    module V1
+        class UsersController < ApplicationController
+            def index
+                users = User.all
+                render json: { status: 'SUCCESS', data:users }, status: :ok
+            end
+
+            def show
+                user = User.find(params[:id])
+                render json: { status: 'SUCCESS', message: 'user loaded', data:user }, status: :ok
+            end
+
+            def create
+                user = User.new(user_params)
+
+                if user.save
+                    render json: { status: 'SUCCESS', message: 'user created!', data:user }, status: :ok
+                else 
+                    render json: { status: 'ERROR', message: 'user not created!', data:user.errors }, status: :unprocessable_entity
+
+                end
+            end
+
+            def destroy
+                user = User.find(params[:id])
+                user.destroy
+                render json: { status: 'SUCCESS', message: 'user deleted!', data:user }, status: :ok
+            end
+
+            def update
+                user = User.find(params[:id])
+                if user.update_attributes(user_params)
+                    render json: { status: 'SUCCESS', message: 'user updated!', data:user }, status: :ok
+                else 
+                    render json: { status: 'ERROR', message: 'user not updated!', data:user.errors }, status: :unprocessable_entity
+
+                end
+            end
+
+            private
+
+            def user_params
+                params.permit(:name, :email)
+            end
+        end
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+5.times do
+    User.create({
+        name: Faker::Name.name, 
+        email: Faker::Internet.email,
+        password: "password"
+    })
+end
