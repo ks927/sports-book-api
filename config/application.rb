02252b4ac434c8ae21fb58e4d740c883951c0d1d
KSheehan <ksheehan927@gmail.com>
2018-03-07 22:23:47 -0500

    CORS set up for frontend

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,13 @@ module SportsBookie
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # allow front end to make api calls from localhost:3000
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
